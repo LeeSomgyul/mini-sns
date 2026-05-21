897ff6615866b668b296b4ba919bd773a68e0d28
leesomgyul <rmawl8600@naver.com>
2026-05-21 16:03:38 +0900

    post: Kafka 메시지로 DB 변경

--- backend/src/main/java/com/example/backend/repository/PostMediaRepository.java
+++ backend/src/main/java/com/example/backend/repository/PostMediaRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PostMediaRepository extends JpaRepository<PostMedia, Long> {
-    Optional<PostMedia> findByPostIdAndMediaType (Long postId, String mediaType);
+    Optional<PostMedia> findByPostIdAndMediaType (Long postId, PostMedia.MediaType mediaType);
+    Optional<PostMedia> findByPostIdAndMediaTypeAndUniqueId(Long postId, PostMedia.MediaType mediaType, String uniqueId);
 }
