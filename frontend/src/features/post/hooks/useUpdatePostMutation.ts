758573a01da0464fa60167f37678d879df527aa4
LeeSomgyul <rmawl8600@naver.com>
2026-06-25 00:56:18 +0900

    fix: 게시물 수정 기능 invalidateQueries 설정 수정

--- frontend/src/features/post/hooks/useUpdatePostMutation.ts
+++ frontend/src/features/post/hooks/useUpdatePostMutation.ts
@@ -1,7 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { PostUpdateParams, PostUpdateRequest } from "../types/postTypes";
 import { postApi } from "../api/postApi";
-import { FEED_KEYS } from "../../../constants/queryKey";
+import { FEED_KEYS, POST_KEYS } from "../../../constants/queryKey";
 import toast from "react-hot-toast";
 
 interface useUpdatePostProps {
@@ -21,9 +21,16 @@ export const useUpdatePostMutation = ({closeModal}: useUpdatePostProps) => {
             };
             return postApi.updatePost(postId,requestBody);
         },
-        onSuccess: () => {
-            // 수정 성공 시 'feeds' 쿼리 캐시 데이터 무효화 및 새로운 데이터로 업데이트
+        //variables: 백엔드에 api 요청했을 때 사용했던 값(PostUpdateParams)
+        onSuccess: (_, variables) => {
+            const {postId} = variables;
+            
+            // 1. 수정 성공 시 'feeds' 쿼리 캐시 데이터 무효화 및 새로운 데이터로 업데이트
             queryClient.invalidateQueries({queryKey: FEED_KEYS.all});
+
+            // 2. 수정 성공 시 'posts' 쿼리 캐시 데이터 무효화 및 새로운 데이터로 업데이트
+            queryClient.invalidateQueries({queryKey: POST_KEYS.detail(postId)});
+
             closeModal();
             toast.success('게시물 수정을 완료하였습니다.');
         },
