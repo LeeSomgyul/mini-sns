995ef417c52ea578450b31946632ec5494f7ad5a
LeeSomgyul <rmawl8600@naver.com>
2026-07-09 23:54:45 +0900

    feat: 댓글 수정 서비스 및 컨트롤러 작업

--- backend/post-module/src/main/java/com/example/backend/controller/CommentManageController.java
+++ backend/post-module/src/main/java/com/example/backend/controller/CommentManageController.java
@@ -1,16 +1,17 @@
 package com.example.backend.controller;
 
 import com.example.backend.dto.ApiResponse;
+import com.example.backend.dto.PostCommentRequest;
 import com.example.backend.jwt.JwtUser;
 import com.example.backend.service.PostCommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.example.backend.dto.PostCommentResponse.*;
 
 @RestController
 @RequestMapping("/v1/comments")
@@ -30,5 +31,19 @@ public class CommentManageController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(ApiResponse.success("댓글이 삭제되었습니다.", null));
+    }
+
+    // [댓글 수정]
+    @PatchMapping("/{commentId}")
+    public ResponseEntity<ApiResponse<CommentContentResponse>> updateComment(
+            @AuthenticationPrincipal JwtUser jwtUser,
+            @PathVariable("commentId") Long commentId,
+            @Valid @RequestBody PostCommentRequest request
+    ){
+        CommentContentResponse response = postCommentService.updateComment(commentId, jwtUser.userId(), request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success("댓글이 수정되었습니다.", response));
     }
 }
