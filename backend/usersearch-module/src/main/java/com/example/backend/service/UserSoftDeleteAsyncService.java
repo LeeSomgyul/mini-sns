75912e681b00b6fb67cd38e7e45eb82f6daa08ac
LeeSomgyul <rmawl8600@naver.com>
2026-07-24 21:49:39 +0900

    fix: 회원탈퇴에 의한 usersearch모듈 레디스 청소 문제 개선

--- backend/usersearch-module/src/main/java/com/example/backend/service/UserSoftDeleteAsyncService.java
+++ backend/usersearch-module/src/main/java/com/example/backend/service/UserSoftDeleteAsyncService.java
@@ -1,6 +1,7 @@
 package com.example.backend.service;
 
 import com.example.backend.repository.UserFollowCacheRepository;
+import com.example.backend.repository.UserSearchRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -16,9 +17,10 @@ import java.util.List;
 public class UserSoftDeleteAsyncService {
 
     private final UserFollowCacheRepository userFollowCacheRepository;
+    private final UserSearchRepository userSearchRepository;
     private final StringRedisTemplate stringRedisTemplate;
 
-    private static final String REDIS_USERSEARCH_FOLLOW_CACHE_PREFIX = "usersearch:following";
+    private static final String REDIS_USERSEARCH_FOLLOW_CACHE_PREFIX = "usersearch:following:";
 
     @Async("usersearchCleanupExecutor")
     @Transactional
@@ -28,24 +30,28 @@ public class UserSoftDeleteAsyncService {
         try{
             // 1. 탈퇴자를 팔로우하고 있던 일반 유저들의 id목록 조회
             List<Long> followerIds = userFollowCacheRepository.findFollowerIdsByFolloweeId(targetUserId);
+            log.info("[1] 조회된 팔로워 IDs: {}", followerIds);
 
             // 2. 탈퇴자를 팔로우하고 있는 사람들의 레디스에서 탈퇴자 제거
             if(!followerIds.isEmpty()){
                 List<String> followerCacheKeys = followerIds.stream()
-                        .map(followerId -> String.format(REDIS_USERSEARCH_FOLLOW_CACHE_PREFIX, followerId))
+                        .map(followerId ->  REDIS_USERSEARCH_FOLLOW_CACHE_PREFIX + followerId)
                         .toList();
 
                 stringRedisTemplate.delete(followerCacheKeys);
             }
 
             // 3. 탈퇴자 본인 레디스 캐시 삭제
-            String myCacheKey = String.format(REDIS_USERSEARCH_FOLLOW_CACHE_PREFIX, targetUserId);
+            String myCacheKey = REDIS_USERSEARCH_FOLLOW_CACHE_PREFIX + targetUserId;
             stringRedisTemplate.delete(myCacheKey);
 
             // 4. DB 삭제
             userFollowCacheRepository.deleteByFollowerIdOrFolloweeId(targetUserId);
 
             // 5. 엘라스틱 서치 인덱스 문서 삭제
+            userSearchRepository.deleteById(targetUserId);
+
+            log.info("[회원탈퇴 비동기 청소 성공] targetUserId: {}", targetUserId);
 
         }catch (Exception e){
             log.info("[회원탈퇴 비동기 청소 실패] targetUserId: {}", targetUserId, e);
